ed3e0095f091eb355c246aa104c69ce8578495c2
PedroLopezITBA <pelopez@itba.edu.ar>
2020-10-30 18:27:17 -0300

    Cosas
    Lul

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -3,8 +3,12 @@
 
 #include <naiveConsole.h>
 #include <keyboard_driver.h>
+#include <video_driver.h>
 #include <timer_driver.h>
 #include <regi.h>
+
+static void int_21h(registerStruct *);
+
 void irqDispatcher(uint64_t irq, uint64_t * registers) {
   // if (irq == 1) {
   //   for (int i = 0; i < 15; i++) {
@@ -14,10 +18,17 @@ void irqDispatcher(uint64_t irq, uint64_t * registers) {
   switch (irq) {
     case 0: timeHandler();
     break;
-    case 1: keyboardHandler((registerStruct *)registers);//printRegisters(registers);
+    case 1: int_21h((registerStruct *)registers);
     break;
   }
 }
+
+static void int_21h(registerStruct * registers) {
+  keyboardHandler(registers);
+  char buffer[1] = {0};
+  readKeyboard(buffer, 1);
+
+};
 
 
 #endif
