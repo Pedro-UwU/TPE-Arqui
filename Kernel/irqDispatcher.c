a46f3f7f7fcd0c3ba119051de2a2c6c84f839452
PedroLopezITBA <pelopez@itba.edu.ar>
2020-10-30 17:52:31 -0300

    Introduccion de keyboard_driver
    Version basica del KBDRIVER >:v

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -5,6 +5,9 @@
 #include <keyboard_driver.h>
 #include <timer_driver.h>
 #include <regi.h>
+
+static void int_21h(registerStruct *);
+
 void irqDispatcher(uint64_t irq, uint64_t * registers) {
   // if (irq == 1) {
   //   for (int i = 0; i < 15; i++) {
@@ -14,10 +17,14 @@ void irqDispatcher(uint64_t irq, uint64_t * registers) {
   switch (irq) {
     case 0: timeHandler();
     break;
-    case 1: keyboardHandler((registerStruct *)registers);//printRegisters(registers);
+    case 1: int_21h((registerStruct *)registers);
     break;
   }
 }
+
+static void int_21h(registerStruct * registers) {
+  keyboardHandler(registers);
+};
 
 
 #endif
