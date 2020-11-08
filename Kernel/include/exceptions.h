b5aa73471e939d67b8b5eb5954c8b0197803fe60
PedroLopezITBA <pelopez@itba.edu.ar>
2020-11-08 15:13:19 -0300

    Arreglo de Warning

--- Kernel/include/exceptions.h
+++ Kernel/include/exceptions.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 void exc_0h(registerStruct * registers);
-void exc_0h(registerStruct * registers);
+void exc_6h(registerStruct * registers);
 void readError(uint64_t * target);
 
 #endif
